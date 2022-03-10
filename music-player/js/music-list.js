015a27fef77cd59de48c6f3fe02bd9d56b247eb4
Paras <shahparas0000@gmail.com>
2022-03-11 02:21:18 +0530

    Files added via upload

---
+++ music-player/js/music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+  {
+    name: "Get Low",
+    artist: "DJ Snake",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Pray For Me",
+    artist: "The Weekend ft. Kendrick Lamar",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Ride It",
+    artist: "Regard",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "In The Dark",
+    artist: "Swae Lee ft. Jhene Aiko",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Shooting Star",
+    artist: "Home Made Kazoku",
+    img: "music-5",
+    src: "music-5"
+  },
+];
